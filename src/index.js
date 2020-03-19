0855a0f6f98564a67453059b9b576983be1d2f17
Favour George <phavorsparks@gmail.com>
2020-03-19 12:40:02 +0100

    setup apollo-client

--- src/index.js
+++ src/index.js
@@ -2,7 +2,11 @@ import React from 'react';
 import { render } from 'react-dom';
 import './styles/index.css';
 import App from './components/App';
+import PersistGate from './config/persistor.config';
 
 const ROOT = document.getElementById('root');
 
-render(<App />, ROOT);
+render(
+  <PersistGate>
+    <App />
+  </PersistGate>, ROOT);
