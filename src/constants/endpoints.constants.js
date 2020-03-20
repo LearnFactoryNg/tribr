0cafd334708e6448b2d7d82129fe6a18562f155d
Favour George <phavorsparks@gmail.com>
2020-03-20 15:26:29 +0100

    update endpoints.constants.js

--- src/constants/endpoints.constants.js
+++ src/constants/endpoints.constants.js
@@ -3,4 +3,4 @@ const LOCAL_SERVER = 'http://localhost:2020/graphql';
 
 const mode = process.env.NODE_ENV === 'production';
 
-export const SERVER_URL = mode ? HEROKU_SERVER : LOCAL_SERVER
+export const SERVER_URL = !mode ? HEROKU_SERVER : LOCAL_SERVER;
